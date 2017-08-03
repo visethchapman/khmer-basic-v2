dc2f1bbdcf773a40dc8e6c29ab30bc89db684349
Viseth Sean <vsean@wpi.edu>
2017-08-02 23:19:32 -0700

    First commit for app Khmer Basic V2

---
+++ app/src/main/java/com/gmail/seanviseth/khmerbasic/Word.java
@@ -0,0 +1,61 @@
+package com.gmail.seanviseth.khmerbasic;
+
+/**
+ * Created by visethsean on 8/2/17.
+ */
+
+public class Word {
+
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private int mAudioResourceId;
+
+    // this constructor for phrase activity
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+    // this constructor for remaining three activities
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceID() {
+        return mImageResourceId;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    // return whether a word has an image
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    // shortcut for Mac: cmd + N to automatically generate below code:
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mAudioResourceId=" + mAudioResourceId +
+                '}';
+    }
+
+}
---
+++ app/src/main/java/com/gmail/seanviseth/khmerbasic/WordAdapter.java
@@ -0,0 +1,86 @@
+package com.gmail.seanviseth.khmerbasic;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by visethsean on 8/2/17.
+ */
+
+public class WordAdapter extends ArrayAdapter {
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentWord = (Word) getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        if (currentWord.hasImage()) {
+            // Get the image resource ID from the current AndroidFlavor object and
+            // set the image to iconView
+            imageView.setImageResource(currentWord.getImageResourceID());
+            // In case it was hidden before
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else{
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
+        // Set the theme color for list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the container View
+        textContainer.setBackgroundColor(color);
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+
+    // Resource ID for the background color for this list of words
+    private int mColorResourceId;
+
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+}
